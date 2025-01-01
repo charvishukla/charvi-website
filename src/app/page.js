126668e6f32e27afda06018a88dfeaf8460dd689
Charvi Shukla <cshukla@ucsd.edu>
2025-01-02 00:47:55 +0530

    Add Experience Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 // pages/index.js
 import styles from "./page.module.css";
-import Image from 'next/image';
+import Image from "next/image";
 import apple from "./Apple-Logosu.png";
 import brain from "./brain.png";
+import wise from "./wise.png";
+
 
 export default function HomePage() {
   return (
@@ -37,31 +39,77 @@ export default function HomePage() {
       <section className={styles.experience}>
         <h2 className={styles.sectionTitle}>Experience</h2>
         <div className={styles.experienceItem}>
-        <Image
-          src={apple}
-          alt="Apple Logo"
-          className={styles.experienceLogo}
-        />
+          <Image
+            src={apple}
+            alt="Apple Logo"
+            className={styles.experienceLogo}
+          />
           <div className={styles.experienceDetails}>
-            <div className={styles.experienceTitle}>Tools and Automation Intern</div>
+            <div className={styles.experienceTitle}>
+              Tools and Automation Intern
+            </div>
             <div className={styles.experienceCompany}>Apple - Core OS Team</div>
-            <div className={styles.experienceDuration}>February 2025 - June 2025</div>
+            <div className={styles.experienceDuration}>
+              February 2025 - June 2025
+            </div>
+            <p className={styles.experienceDescription}>
+              Incoming full-time intern for Winter and Spring quarters in 2025.
+            </p>
           </div>
         </div>
+        
+        
         <div className={styles.experienceItem}>
-        <Image
-          src={brain}
-          alt="Brain Corp Logo"
-          className={styles.experienceLogoBrain}
-        />
+          <Image
+            src={brain}
+            alt="Brain Corp Logo"
+            className={styles.experienceLogoBrain}
+          />
           <div className={styles.experienceDetails}>
-            <div className={styles.experienceTitle}>Software Engineering Intern</div>
-            <div className={styles.experienceCompany}>Brain Corporation - Sense Tools Team</div>
-            <div className={styles.experienceDuration}> June 2025 - September 2025</div>
+            <div className={styles.experienceTitle}>
+              Software Engineering Intern
+            </div>
+            <div className={styles.experienceCompany}>
+              Brain Corporation - Sense Tools Team
+            </div>
+            <div className={styles.experienceDuration}>
+              {" "}
+              June 2025 - September 2025
+            </div>
+            <p className={styles.experienceDescription}>
+            Incoming full-time intern for Summer 2025. 
+            </p>
           </div>
         </div>
-      </section>
 
+        <div className={styles.experienceItem}>
+          <Image
+            src={wise}
+            alt="WC Logo"
+            className={styles.experienceLogoWise}
+          />
+          <div className={styles.experienceDetails}>
+            <div className={styles.experienceTitle}>
+              Software Engineering Intern
+            </div>
+            <div className={styles.experienceCompany}>
+              Wise Counsel AI - Core Negotiation Engine
+            </div>
+            <div className={styles.experienceDuration}>
+              {" "}
+              November 2023 - April 2024
+            </div>
+            <ul className={styles.experienceDescription}>
+            <li>Built CLI for WiseCounsel's negotiation platform enabling client-counterparty communication </li>
+            <li>Designed a deterministic state machine to enable verification and E2E testing of negotiation workflow</li>
+            <li>Orchestrated multi-clause negotiations by managing 10+ inputs/outputs within a agreement lifecycle from 4 distinct AI
+            Agents to reconstruct legal agreements</li>
+            </ul>
+          </div>
+        </div>
+
+
+      </section>
 
       <section className={styles.research}>
         <h2 className={styles.sectionTitle}>Research</h2>
